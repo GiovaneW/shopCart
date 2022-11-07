4fc8baaa202707f44d064e025c1da191f8ab09f8
Giovane Welter <giovane.welter@nuvoni.com.br>
2022-11-06 22:40:24 -0300

    feature: start config

---
+++ src/routes/product.Routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import { ProductController } from '../controllers/productController'
+
+export class ProductRoutes {
+    public router: Router
+    private productCtl: ProductController
+    constructor() {
+        this.router = Router()
+        this.productCtl = new ProductController()
+        this.routes()
+    }
+
+    private routes(): void {
+        this.router.get('/', this.productCtl.listProducts)
+        this.router.get('/:id', this.productCtl.getProduct)
+        this.router.post('/', this.productCtl.registerProduct)
+        this.router.patch('/:id', this.productCtl.updateProduct)
+        this.router.put('/:id', this.productCtl.changeProduct)
+    }
+}
